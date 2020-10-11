063e5e3b0bc74fc84af1bbd42db4752fe0e6d9d1
Jimmy Kivlen <jimmykivlen@gmail.com>
2020-10-11 00:34:33 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,12 +3,18 @@ class Dog
   attr_accessor :owner, :mood
   attr_reader :name
   
+  @@all = []
+  
   def initialize(name, owner, mood="nervous")
     @name = name
     @owner = owner
     @mood = mood
+    @@all << self
   end
   
+  def self.all
+    @@all
+  end
   
   
 end
