ea8b9900943f357cfab35d41d9bb47223b331a20
Jimmy Kivlen <jimmykivlen@gmail.com>
2020-10-11 00:51:39 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -61,7 +61,8 @@ class Owner
   def sell_pets
     pets  = self.dogs + self.cats
     
-    pets.each { |pet| pet.mood = "nervous"}
+    pets.each { |pet| pet.mood = "nervous"
+    pet.owner = nil}
   end
   
   
