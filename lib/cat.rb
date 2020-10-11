f5ecc7b7398ec2094a2a05cf14258248b2327c67
Jimmy Kivlen <jimmykivlen@gmail.com>
2020-10-11 00:30:35 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -3,12 +3,20 @@ class Cat
   attr_accessor :owner, :mood
   attr_reader :name
   
+  @@all = []
+  
+  
   def initialize(name, owner, mood = "nervous")
     @name = name
     @owner = owner
     @mood = mood
+    @@all << self
   end
   
+  
+  def self.all
+    @@all
+  end
   
   
   
