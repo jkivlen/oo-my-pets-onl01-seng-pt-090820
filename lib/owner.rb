e8394e8a53fa9a3e61ce368cd948a11b105f53fa
Jimmy Kivlen <jimmykivlen@gmail.com>
2020-10-11 00:23:48 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,9 +1,17 @@
 class Owner
   attr_reader :name, :species
   
+  @all = []
+  
+  
   def initialize(name, species="human")
     @name = name
     @species = species
+    @all << self
+  end
+  
+  def self.all
+    @all
   end
   
   def name
@@ -16,7 +24,6 @@ class Owner
   
   def say_species
     "I am a #{species}."
-    
   end
   
   
